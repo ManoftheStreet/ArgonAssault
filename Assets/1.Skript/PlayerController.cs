871ce63dea050c6a447190e036f447fe04581f70
ManoftheStreet <zhijian6527@naver.com>
2024-01-02 17:34:25 +0900

    타임라인 애니메이션, terrain 설정,

---
+++ Assets/1.Skript/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float horizontalThrow = Input.GetAxis("Horizontal");
+        Debug.Log(horizontalThrow);
+        float verticalThrow = Input.GetAxis("Vertical");
+        Debug.Log(verticalThrow);
+
+    }
+}
