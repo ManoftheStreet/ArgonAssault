9e610d7f33445c2815d3183a7b610de775e911c5
Geon <zhijian6527@naver.com>
2024-01-03 22:55:10 +0900

    새로운인풋매니저 적용해봄/VR컨트롤처럼 바인딩 하는 느낌

--- Assets/1.Skript/PlayerController.cs
+++ Assets/1.Skript/PlayerController.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
 
     void Start()
     {
 
     }
 
+    private void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+    }
+
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+
+        /*float horizontalThrow = Input.GetAxis("Horizontal");
         Debug.Log(horizontalThrow);
         float verticalThrow = Input.GetAxis("Vertical");
+        Debug.Log(verticalThrow);*/
+        Debug.Log(horizontalThrow);
         Debug.Log(verticalThrow);
 
     }
